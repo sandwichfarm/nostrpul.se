117d9d086b7d85ed733f19cbece0a001b62d1e9c
sandwich <299465+dskvr@users.noreply.github.com>
2024-02-01 15:13:50 +0000

    add paths.base to conf

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  paths: {
-    base: '/nip66-dataviz-demo/', // Set this to your subdirectory
-  },
+  kit: {
+    paths: {
+      base: '/nip66-dataviz-demo/', // Set this to your subdirectory
+    },
+  }
 }
