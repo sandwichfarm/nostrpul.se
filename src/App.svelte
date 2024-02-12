694d93c2b7712db7b7f7e77cdb88507188a0ae35
sandwich <299465+dskvr@users.noreply.github.com>
2024-02-12 21:11:03 +0000

    add modal change stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,34 @@
 <script>
-import { onMount, afterUpdate, tick } from 'svelte';
+  
+  
+  import { onMount, afterUpdate, tick } from 'svelte';
+  import Modal from './Modal.svelte';
   import { Relay } from 'nostr-tools';
   import { fade } from 'svelte/transition';
   import Masonry from 'masonry-layout';  
   import { writable } from 'svelte/store';
 
+  let currentModal = null;
+
   const events = writable([]);
   let loading = true;
   let masonry;
   let firstEoseReceived = false;
-  let since = Math.round(Date.now() / 1000) - (60 * 60 * 2)
+  let since = Math.round(Date.now() / 1000) - (60 * 60 * 1.1)
+
+  // let currentModal = null; 
+
+  function showModal(id) {
+    currentModal = id;
+  }
+
+  function hideModal() {
+    currentModal = null;
+  }
 
   function processEvent(event) {
     const dTag = event.tags.find(tag => tag[0] === 'd');
-    if (!dTag) return null; // Skip if no 'd' tag
+    if (!dTag) return null; 
 
     return {
       ...event,
@@ -35,7 +50,7 @@ import { onMount, afterUpdate, tick } from 'svelte';
   });
 
   function initializeMasonry() {
-    masonry = new Masonry('.main', {
+    masonry = new Masonry('#events', {
       itemSelector: '.event',
       percentPosition: true,
       horizontalOrder: true
@@ -43,10 +58,11 @@ import { onMount, afterUpdate, tick } from 'svelte';
   }
 
   const connect = async () => {
-    const relay = await Relay.connect('wss://history.nostr.watch');
+    const relay = await Relay.connect('wss://history.nostr.watch').catch(connect);
     relay.subscribe(
       [
         {
+          limit: 1000,
           kinds: [30066],
           authors: ['cd18a5109bd5a3110e173331d873725dbf0c5bedc9357a3cc80ed7029b24e974'],
           since
@@ -54,6 +70,7 @@ import { onMount, afterUpdate, tick } from 'svelte';
       ],
       {
         onevent(event) {  
+          if(firstEoseReceived && event.created_at < since) return
           const processedEvent = processEvent(event);
           since = event.created_at
           if (processedEvent) {
@@ -66,20 +83,21 @@ import { onMount, afterUpdate, tick } from 'svelte';
           console.log(event.id)
         },
         onclose() {
-          console.log('Subscription closed');
-          connect()
+          const reconnect = 2000
+          console.log(`Subscription closed, reconnecting in ${reconnect}`);
+          setTimeout( connect, reconnect )
         },
         oneose() {
           console.log('EOSE')
           processBatch();
-          firstEoseRecieved=true
+          firstEoseReceived=true
         }
       }
     );
   }
 
-  onMount(() => {
-    connect()
+  onMount(async () => {
+    await connect()
 
     return () => {
       relay.close();
@@ -122,6 +140,18 @@ import { onMount, afterUpdate, tick } from 'svelte';
     return color;
   }
 
+  function getRttFromEvent(key, event){
+    const rtt = event.tags.filter(tag => tag[0] === 'rtt' && tag[1] === key)?.[0]?.[2]
+    return rtt? rtt: ''
+  }
+
+  function rttDisplay(key, event){
+    const rtt = getRttFromEvent(key, event)
+    const letter = key ? key.charAt(0).toUpperCase() : '';
+    return rtt? `${letter}:${rtt}ms`: ''
+  }
+
+
   function generateColorFromTag(str) {
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
@@ -140,6 +170,42 @@ import { onMount, afterUpdate, tick } from 'svelte';
     return color;
 }
 
+function parse30066(event) {
+    const parsedTags = {};
+    const url = event.tags.filter( tag => tag[0] == 'd')[0][1]
+    event.tags = event.tags.filter( tag => tag.length >= 3)
+    event.tags.forEach(tag => {
+        const [key, subkey, ...values] = tag;
+        if (!parsedTags[key]) {
+            parsedTags[key] = {};
+        }
+        if (!parsedTags[key][subkey]) {
+            parsedTags[key][subkey] = values.length > 1 ? values.map(v => castValue(v)) : castValue(values[0]);
+        }
+        else {
+            if (Array.isArray(parsedTags[key][subkey])) {
+                parsedTags[key][subkey] = [...parsedTags[key][subkey], ...values.map(v => castValue(v))];
+            }
+            else {
+                parsedTags[key][subkey] = [parsedTags[key][subkey], ...values.map(v => castValue(v))];
+            }
+        }
+    });
+    return { url, ...parsedTags};
+}
+function castValue(value) {
+    if (value.toLowerCase() === 'true')
+        return true;
+    if (value.toLowerCase() === 'false')
+        return false;
+    const asFloat = parseFloat(value);
+    if (!isNaN(asFloat) && isFinite(asFloat)) {
+        return asFloat;
+    }
+    return value;
+}
+
+
 
   function generateBackground(event) {
     const dTag = event.tags.find(tag => tag[0] === 'd');
@@ -154,18 +220,42 @@ import { onMount, afterUpdate, tick } from 'svelte';
 </script>
 
 {#if firstEoseReceived}
-  
-  <main class="main section">
-    <div id="stats">
-      <small>relays:</small> <br />
-      {$events.length} online
+  <div id="stats">
+    <div id="online" class="metric">
+      <span class="key">online</span>
+      <span class="value">{$events.length}</span>
     </div>
-    {#each $events as event (event.id)}
-      <div class="event" style={`height: ${event.dimension}px; background-color: ${event.backgroundColor};`} in:fade>
-        {event.tags.filter(tag => tag[0] === 'rtt' && tag[1] === 'open')[0][2]}
-        <span style="display:none">{JSON.stringify(event, null, 4)}</span>
-      </div>
-    {/each}
+    <div id="readable" class="metric">
+      <span class="key">readable</span>
+      <span class="value">{$events.filter(event => event.tags.filter( tag => tag[0] === 'rtt' && tag[1] === 'read').length > 0 ).length}</span>
+    </div>
+    <div id="writable" class="metric">
+      <span class="key">writable</span>
+      <span class="value">{$events.filter(event => event.tags.filter( tag => tag[0] === 'rtt' && tag[1] === 'write').length > 0 ).length}</span>
+    </div>
+  </div>
+  <main class="main section">
+    <div id="events">
+      
+      {#each $events as ev (ev.id)}
+        <div class="event" 
+            style={`height: ${ev.dimension}px; background-color: ${ev.backgroundColor};`} 
+            in:fade 
+            on:click={() => showModal(ev.id)} 
+            role="button" 
+            tabindex="0" 
+            on:keydown={(event) => event.key === 'Enter' && showModal(ev.id)}>
+          <span class="rtt">{rttDisplay('open', ev)}</span>
+          <span class="rtt">{rttDisplay('read', ev)}</span>
+          <span class="rtt">{rttDisplay('write', ev)}</span>
+        </div>
+        {#if currentModal === ev.id}
+          <Modal showModal={true} on:close={hideModal}>
+            <pre>{JSON.stringify(parse30066(ev), null, 4)}</pre>
+          </Modal>
+        {/if}
+      {/each}
+    </div>
   </main>
 {:else}
   <div id="loading">loading</div>
@@ -178,11 +268,11 @@ import { onMount, afterUpdate, tick } from 'svelte';
     padding:0;
   }
   .main.section {
-    position: fixed; /* Ensure it's fixed to the viewport */
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    position: fixed !important; /* Ensure it's fixed to the viewport */
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
     overflow: hidden;  /* Prevent scrollbars within this section */
     z-index: 1;  /* Ensure it's above other content */
   }
@@ -201,17 +291,52 @@ import { onMount, afterUpdate, tick } from 'svelte';
     transition: opacity 0.3s ease-out;
   }
 
+  .event > .rtt {
+    display:none;
+  }
+
+  .event:hover > .rtt {
+    display:block;
+    color:black;
+    cursor:pointer;
+    font-family: monospace
+  }
+
   #loading { 
     font-size:50em;
     color:rgba(0,0,0,0.05)
   }
 
   #stats {
-    font-size:10em;
-    position:absolute;
-    top:100px;
-    left:0px; 
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    display: flex; /* Use flex display */
+    align-items: center; /* Align items vertically in the center */
+    justify-content: center; /* Center the items horizontally (if needed) */
+    color: rgba(0, 0, 0, 0.5);
+    transform: translateY(-50%); /* Adjust top positioning to truly center */
+  }
+
+  #stats .metric {
+    width: 33%;
+    display: flex; /* Use flex display */
+    flex-direction: column; /* Stack children vertically */
+    justify-content: center; /* Center the content vertically */
+    text-align: center;
+  }
+
+
+  #stats .metric > * {
     display:block;
-    color:rgba(0,0,0,0.5);
+  }
+
+  #stats .metric .key {
+    font-size:5em
+  }
+
+  #stats .metric .value {
+    font-size:20em
   }
 </style>
